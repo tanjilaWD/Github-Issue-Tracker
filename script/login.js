145d779b74fe89a80866e7e8c3231dac5ce355e9
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-03-07 18:06:23 +0600

    feat: sign in js code

---
+++ script/login.js
@@ -0,0 +1,21 @@
+document.getElementById('SignIn-btn').addEventListener('click', function(){
+    const userName = document.getElementById('userName');
+    const user = userName.value ;
+    console.log(user);
+
+    const userPass = document.getElementById('user_pass');
+    const password = userPass.value;
+    console.log(password);
+
+    document.getElementById('userName').value = '';
+    document.getElementById('user_pass').value = '';
+
+    if(user == 'admin' && password =='admin123'){
+        window.location.assign('../home.html');
+    }
+    else{
+        alert('Try Failed');
+        return;
+    }
+    
+});
